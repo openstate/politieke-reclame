2b5f08379e2015d48174b8183f81ca105ab2a330
Sicco van Sas <siccovansas@users.noreply.github.com>
2025-10-08 18:02:32 +0200

    Small fix

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ cd /opt/politieke-reclame
 
 git config --global user.email "developers@openstate.eu"
 git config --global user.name "Open State developers"
+git config --global --add safe.directory /opt/politieke-reclame
+git add items/
 git commit -m 'Statements changed' && git push -q https://`cat GITHUB_PERSONAL_TOKEN_FILE`@github.com/openstate/politieke-reclame.git main
