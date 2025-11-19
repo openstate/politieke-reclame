9438bf9078a3b6da23c9d9f01d3f8bb81f873bfb
Sicco van Sas <siccovansas@users.noreply.github.com>
2025-11-19 10:52:29 +0100

    Small fix

--- get-statements.py
+++ get-statements.py
@@ -4,12 +4,12 @@ import requests
 from slugify import slugify
 
 limit = 100
-page = 0
+page = 1
 
 response = requests.get(f'https://politiekereclame.nl/api/public/statements?page={page}&limit={limit}')
 data = response.json()
 
-while data['meta']['filter_count'] > (limit * page - 1):
+while data['meta']['filter_count'] > (limit * (page - 1)):
   for item in data['items']:
     org = slugify(item['organization_created'].get('name', ''))
     name = slugify(item.get('name', ''))
